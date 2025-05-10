5c7f81b6820e858fcd368beb8a7c42f16afe6d54
SantStyle <134206753+santstyle@users.noreply.github.com>
2025-05-10 20:39:30 +0700

    Add files via upload

--- owner/script.js
+++ owner/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const followBtn = document.getElementById('followBtn');
-  followBtn.addEventListener('click', function () {
-    if (followBtn.classList.contains('following')) {
-      followBtn.classList.remove('following');
-      followBtn.textContent = 'Follow';
-    } else {
-      followBtn.classList.add('following');
-      followBtn.textContent = 'Following';
-    }
-  });
+    const followBtn = document.getElementById('followBtn');
+    followBtn.addEventListener('click', function () {
+        if (followBtn.classList.contains('following')) {
+            followBtn.classList.remove('following');
+            followBtn.textContent = 'Follow';
+        } else {
+            followBtn.classList.add('following');
+            followBtn.textContent = 'Following';
+        }
+    });
 });
